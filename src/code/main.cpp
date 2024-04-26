f9a9d98a5221d4714178a4706b26d7e2dac5e1df
Fermin Oloriz <ferminoloriz@gmail.com>
2024-04-25 19:01:36 -0700

    Shader and program woek

--- src/code/main.cpp
+++ src/code/main.cpp
@@ -1,11 +1,20 @@
+/*
+ * TODO(Fermin):
+ * Import Buffer.cpp
+ * Stop malloc
+ *
+ * FIX why shader compilation fails sometimes
+ *
+*/
 #include <cstdio>
+#include <cmath>
 
 // NOTE(Fermin): Include glad before glfw3
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "glad.c"
 
-typedef int32_t i32;
+typedef int32_t  i32;
 
 typedef uint8_t  u8;
 typedef uint32_t u32;
@@ -16,32 +25,130 @@ typedef float    f32;
 typedef double   f64;
 
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }  
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+static u32 compile_shader(i32 type, char** source)
+{
+    u32 id;
+    id = glCreateShader(type);
+    glShaderSource(id, 1, source, NULL);
+    glCompileShader(id);
 
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0";
+    i32 success;
+    char infoLog[512];
+    glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(id, 512, NULL, infoLog);
+        fprintf(stderr, "ERROR::SHADER::%s::COMPILATION_FAILED\n%s", type == GL_VERTEX_SHADER ? "VERTEX" : "FRAGMENT", infoLog);
+    }
 
-const char *fragmentShaderSource2 = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "    FragColor = vec4(1.0f, 0.0f, 1.0f, 1.0f);\n"
-    "}\0";
+    return id;
+}
 
+static long open_file(FILE **handle, char const *name)
+{
+    *handle = fopen(name, "rb");
+    if (!handle)
+    {
+        fprintf(stderr, "Error: Couldn't open file: %s\n", name);
+        return -1;
+    }
+
+    fseek(*handle, 0, SEEK_END);
+    long file_size = ftell(*handle);
+    rewind(*handle);
+
+    return file_size;
+}
+
+static void read_file(char **buffer, const char *source)
+{
+    FILE* file;
+    long file_size = open_file(&file, source);
+
+    *buffer = (char*)malloc(file_size);
+    if (*buffer)
+    {
+        size_t bytes_read = fread(*buffer, sizeof(char), file_size, file);
+        if (bytes_read != file_size)
+        {
+            fprintf(stderr, "Error: Failed to read %s\nfile_size: %lu, bytes_read: %zu", source, file_size, bytes_read);
+            free(*buffer);
+            fclose(file);
+        }
+        fclose(file);
+    }
+    else
+    {
+        fprintf(stderr, "Error: Memory allocation failed.\n");
+        fclose(file);
+    }
+}
+
+/*
+#include <stdio.h>
+#define WINDOWS 
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
+
+char *buf;
+buf=(char *)malloc(100*sizeof(char));
+getcwd(buf,100);
+printf("\n %s \n",buf);
+*/
+
+struct Program
+{
+    u32 id;
+    char const* vertex_shader;
+    char const* fragment_shader;
+};
+
+static void build_program(Program *program)
+{
+    char* vertex_shader_source;
+    read_file(&vertex_shader_source, program->vertex_shader);
+
+    char* fragment_shader_source;
+    read_file(&fragment_shader_source, program->fragment_shader);
+
+    u32 vertex_shader_id = compile_shader(GL_VERTEX_SHADER, &vertex_shader_source);
+    u32 fragment_shader_id = compile_shader(GL_FRAGMENT_SHADER, &fragment_shader_source);
+
+    free(vertex_shader_source);
+    free(fragment_shader_source);
+
+    u32 program_id;
+    program_id = glCreateProgram();
+    glAttachShader(program_id, vertex_shader_id);
+    glAttachShader(program_id, fragment_shader_id);
+    glLinkProgram(program_id);
+
+    i32 success;
+    char infoLog[512];
+    glGetProgramiv(program->id, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(program_id, 512, NULL, infoLog);
+        fprintf(stderr, "ERROR::SHADER::LINKING_FAILED\n%s", infoLog);
+    } 
+    glUseProgram(program_id);
+
+    glDeleteShader(vertex_shader_id);
+    glDeleteShader(fragment_shader_id); 
+
+    program->id = program_id;
+}
 
 int main()
 {
@@ -71,89 +178,21 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
     // NOTE(Fermin) | End | Init window and opengl
 
-
     // NOTE(Fermin) | Start | Shaders and Program
-    u32 vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    i32 v_success;
-    char v_infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &v_success);
-    if(!v_success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, v_infoLog);
-        fprintf(stderr, "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", v_infoLog);
-    }
+    Program test_program = {};
+    test_program.vertex_shader = "src\\code\\vertex.vert";
+    test_program.fragment_shader = "src\\code\\fragment.frag";
 
-    u32 fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    i32 f_success;
-    char f_infoLog[512];
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &f_success);
-    if(!f_success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, f_infoLog);
-        fprintf(stderr, "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s", f_infoLog);
-    }
-
-    u32 fragmentShader2;
-    fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
-    glCompileShader(fragmentShader2);
-    i32 f_success2;
-    char f_infoLog2[512];
-    glGetShaderiv(fragmentShader2, GL_COMPILE_STATUS, &f_success2);
-    if(!f_success2)
-    {
-        glGetShaderInfoLog(fragmentShader2, 512, NULL, f_infoLog2);
-        fprintf(stderr, "ERROR::SHADER::FRAGMENT2::COMPILATION_FAILED\n%s", f_infoLog2);
-    }
-
-    u32 shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    i32 p_success;
-    char p_infoLog[512];
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &p_success);
-    if(!p_success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, p_infoLog);
-        fprintf(stderr, "ERROR::SHADER::LINKING_FAILED\n%s", p_infoLog);
-    } 
-    glUseProgram(shaderProgram);
-
-    u32 shaderProgram2;
-    shaderProgram2 = glCreateProgram();
-    glAttachShader(shaderProgram2, vertexShader);
-    glAttachShader(shaderProgram2, fragmentShader2);
-    glLinkProgram(shaderProgram2);
-    i32 p_success2;
-    char p_infoLog2[512];
-    glGetProgramiv(shaderProgram2, GL_LINK_STATUS, &p_success2);
-    if(!p_success2)
-    {
-        glGetProgramInfoLog(shaderProgram2, 512, NULL, p_infoLog2);
-        fprintf(stderr, "ERROR::SHADER::LINKING_FAILED\n%s", p_infoLog2);
-    } 
-    glUseProgram(shaderProgram2);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader); 
-    glDeleteShader(fragmentShader2); 
+    build_program(&test_program);
     // NOTE(Fermin) | End | Shaders and Program
 
     // NOTE(Fermin) | Start | VAO, VBO. EBO
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    }; 
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,   // first triangle
         1, 2, 3    // second triangle
@@ -177,8 +216,12 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0); 
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // NOTE(Fermin): that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
@@ -189,26 +232,43 @@ int main()
     glBindVertexArray(0); 
     // NOTE(Fermin) | End | VAO, VBO. EBO
 
+    b32 wireframe_mode = 0;
+    // NOTE(Fermin): OFC we need an actual key struct for these...
+    b32 f1_key_state = 0; // NOTE(Fermin): 0 released else pressed
     while(!glfwWindowShouldClose(window))
     {
         if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, true);
 
+        if(glfwGetKey(window, GLFW_KEY_F1) == GLFW_PRESS && !f1_key_state)
+            wireframe_mode = !wireframe_mode;
+            f1_key_state = 1;
+
+        if(glfwGetKey(window, GLFW_KEY_F1) == GLFW_RELEASE)
+            f1_key_state = 0;
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // NOTE(Fermin): This is what we need to set to specify what and how to draw
-        glUseProgram(shaderProgram);
+        glUseProgram(test_program.id);
         glBindVertexArray(VAO);
 
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        if(true) //NOTE(Fermin): Check for wireframe mode
+        if(wireframe_mode)
         {
-            glUseProgram(shaderProgram2);
+            // TODO(Fermin): We need a different color for the wireframe
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+            // NOTE(Fermin): sets the uniform on the CURRENTLY ACTIVE shader program.
+            //int vertexColorLocation = glGetUniformLocation(shaderProgram, "wireframeColor");
+            //glUniform4f(vertexColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         }
 
