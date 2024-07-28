1cd080ed67a245abca5ce1d90e802b4900bbe9fe
Fermin Oloriz <ferminoloriz@gmail.com>
2024-07-28 13:59:22 -0700

    Build: Change the build script so it no longer pushes and pops the directory when building to avoid the different path on error files. This change was done because now I'm using quickfix list for errors and the paths mismatched with the working directory of vim

--- src/code/build.bat
+++ src/code/build.bat
@@ -16,11 +16,11 @@ if "%Platform%" neq "x64" (
 )
 
 REM TODO(Fermin): Test by moving glfw lib to the project
-set CommonCompilerFlags= -nologo -Zi /EHsc /I ..\src\include 
-set CommonLinkerFlags= /NODEFAULTLIB:MSVCRTD /NODEFAULTLIB:LIBCMT -incremental:no -opt:ref ..\libs\glfw3.lib opengl32.lib msvcrt.lib Gdi32.lib User32.lib shell32.lib
+set CommonCompilerFlags= -nologo -Zi /EHsc /I src\include /Fobuild\ 
+set CommonLinkerFlags= /NODEFAULTLIB:MSVCRTD /NODEFAULTLIB:LIBCMT -incremental:no -opt:ref libs\glfw3.lib opengl32.lib msvcrt.lib Gdi32.lib User32.lib shell32.lib
 
 if not exist .\build mkdir .\build
-pushd build
+REM pushd build
 
 echo -------------------------------------
 echo Building with MSVC
@@ -33,12 +33,12 @@ for /f "tokens=1-3 delims=/:" %%a in ("%TIME%")  do (set starttime=%%a:%%b:%%c)
 @echo:
 
 REM delete pdbs each compilation since we make a new one each time we compile for live code editing and send output streams to NUL so it doesnt echo
-del *.pdb > NUL 2> NUL
+del build\*.pdb > NUL 2> NUL
 
-call cl %CommonCompilerFlags% ..\src\code\asset_builder.cpp  /link %CommonLinkerFlags% -OUT:asset_builder.exe
-call cl %CommonCompilerFlags% ..\src\code\game.cpp -LD /link -incremental:no -opt:ref -PDB:game_%date:~-8,2%%date:~-5,2%%date:~-2,2%_%time:~-11,2%%time:~-8,2%%time:~-5,2%%time:~-2,2%.pdb -EXPORT:game_update_and_render
-call cl %CommonCompilerFlags% ..\src\code\windows_main.cpp  /link %CommonLinkerFlags% -OUT:windows_main_debug.exe
-call cl -O2 %CommonCompilerFlags% ..\src\code\windows_main.cpp  /link %CommonLinkerFlags% -OUT:windows_main_release.exe
+call cl %CommonCompilerFlags% /Febuild\asset_builder.exe src\code\asset_builder.cpp  /link %CommonLinkerFlags% 
+call cl %CommonCompilerFlags% src\code\game.cpp -LD /link /IMPLIB:build\game.lib /OUT:build\game.dll -incremental:no -opt:ref -PDB:build\game_%date:~-8,2%%date:~-5,2%%date:~-2,2%_%time:~-11,2%%time:~-8,2%%time:~-5,2%%time:~-2,2%.pdb -EXPORT:game_update_and_render
+call cl %CommonCompilerFlags% /Febuild\windows_main_debug.exe src\code\windows_main.cpp  /link %CommonLinkerFlags% 
+call cl -O2 %CommonCompilerFlags% /Febuild\windows_main_release.exe src\code\windows_main.cpp  /link %CommonLinkerFlags% 
 REM del lock.tmp
 
 for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set finishdate=%%b-%%a-%%c)
@@ -46,7 +46,7 @@ for /f "tokens=1-3 delims=/:" %%a in ("%TIME%") do (set finishtime=%%a:%%b:%%c)
 @echo:
 @echo Compilation finished %finishdate% at %finishtime%
 
-popd
+REM popd
 
 
 
